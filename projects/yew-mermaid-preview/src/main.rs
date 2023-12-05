6e9e21391de7a729023b00b85609d13044553b47
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2023-12-05 16:52:55 -0500

    fix up the merms

--- projects/yew-mermaid-preview/src/main.rs
+++ projects/yew-mermaid-preview/src/main.rs
@@ -1,7 +1,30 @@
 #![recursion_limit = "1024"]
 
-use yew::{html, Html};
+use web_sys::{
+    console, wasm_bindgen::JsCast, EventTarget, HtmlInputElement, HtmlTextAreaElement,
+};
+use yew::{function_component, html, use_state, Callback, Html, InputEvent};
 use yew_mermaid::Mermaid;
+
+const CODE: &str = r#"
+sequenceDiagram
+    Perseus->>Server: Login
+    Server->>Perseus: login url with user login state cookie
+    Perseus->>Keycloak: Authenticate with Keycloak
+    Keycloak->>Perseus: Redirection with code,state,etc
+    Perseus->>Server: Exchange code, validate csrf
+    Server-->>Server: Record user data
+    Server-->>Perseus: User data with session
+    Perseus-->>Perseus: App Unlock
+    loop Data Requests with Session
+        Perseus-->>Server: Catalogs/Schemas/Tables/Query
+        Server-->>Keycloak: User Validation
+        Keycloak-->>Server: User Validated
+        Server-->>Scheduler: Request for Data
+        Scheduler-->>Server: Response with Data
+        Server-->>Perseus: Data
+    end
+"#;
 
 pub fn header_view() -> Html {
     let title = "Mermaid.js for Yew";
@@ -17,6 +40,44 @@ pub enum Event {
     Input(String),
 }
 
+#[function_component(App)]
+pub fn app() -> Html {
+    let code = use_state(String::default);
+    let code_setter = code.setter();
+
+    let oninput = {
+        Callback::from(move |e: InputEvent| {
+            let target: Option<EventTarget> = e.target();
+            let input = target.and_then(|t| t.dyn_into::<HtmlTextAreaElement>().ok());
+            if let Some(input) = input {
+                code_setter.set(input.value());
+            }
+        })
+    };
+
+    html! {
+        <>
+            {header_view()}
+            <main>
+            <textarea
+                columns="20"
+                placeholder="Input mermaid code"
+                value={(*code).clone()}
+                {oninput}
+            />
+            <div>
+                <label>
+                    {"Mermaid output svg:"}
+                </label>
+            </div>
+            if !code.is_empty() {
+                <Mermaid code={(*code).clone()} />
+            }
+            </main>
+        </>
+    }
+}
+
 pub struct Model {}
 //     link: ComponentLink<Self>,
 //     storage: StorageService,
@@ -28,7 +89,6 @@ pub struct Model {}
 //     type Properties = ();
 
 //     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-//         let storage = StorageService::new(Area::Local).expect("storage was unavailable!");
 //         let input = {
 //             match storage.restore("mermaid") {
 //                 Json(Ok(restored)) => restored,
@@ -71,5 +131,5 @@ pub struct Model {}
 // }
 
 fn main() {
-    // yew::start_app::<Model>();
+    yew::Renderer::<App>::new().render();
 }
--- projects/yew-mermaid/src/mermaid/mod.rs
+++ projects/yew-mermaid/src/mermaid/mod.rs
@@ -1,4 +1,5 @@
 pub use mermaid_wasmbind::MermaidOptions;
+use web_sys::{wasm_bindgen::UnwrapThrowExt, Element, HtmlElement};
 use yew::{prelude::*, Component, Html};
 
 #[derive(Properties, Clone, PartialEq, Default)]
@@ -10,46 +11,68 @@ pub struct MermaidProperties {
     pub theme: String,
 }
 
-#[derive(Default)]
-pub struct Mermaid {
-    pub props: MermaidProperties,
-}
+#[function_component(Mermaid)]
+pub fn mermaid(props: &MermaidProperties) -> Html {
+    let node = use_node_ref();
 
-impl Component for Mermaid {
-    type Message = ();
-    type Properties = MermaidProperties;
+    {
+        let node = node.clone();
+        let mut opts = MermaidOptions::default();
+        opts.set_theme(&props.theme);
+        let code = props.code.clone();
 
-    fn create(ctx: &Context<Mermaid>) -> Self {
-        Self::default()
+        use_effect_with((), move |_| {
+            let elem: Element = node.cast().expect_throw("failed to get mermaid element");
+            let x = opts.render(&elem, &code);
+            elem.set_inner_html(&x);
+        });
     }
 
-    fn update(&mut self, ctx: &yew::Context<Mermaid>, _: Self::Message) -> bool {
-        false
-    }
-
-    fn changed(&mut self, ctx: &yew::Context<Mermaid>, props: &MermaidProperties) -> bool {
-        match &self.props == props {
-            true => false,
-            false => {
-                self.props = props.clone();
-                true
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Mermaid>) -> Html {
-        let drawer = self.create_drawer();
-        let document = gloo_utils::document();
-        let t = document.create_element("div").unwrap();
-        t.set_inner_html(&drawer.render(&t, &self.props.code));
-        Html::VRef(t.first_child().unwrap().into())
+    html! {
+        <div ref={node}></div>
     }
 }
 
-impl Mermaid {
-    pub fn create_drawer(&self) -> MermaidOptions {
-        let mut render = MermaidOptions::default();
-        render.set_theme(&self.props.theme);
-        return render;
-    }
-}
+// #[derive(Default)]
+// pub struct Mermaid {
+//     pub props: MermaidProperties,
+// }
+
+// impl Component for Mermaid {
+//     type Message = ();
+//     type Properties = MermaidProperties;
+
+//     fn create(ctx: &Context<Mermaid>) -> Self {
+//         Self::default()
+//     }
+
+//     fn update(&mut self, ctx: &yew::Context<Mermaid>, _: Self::Message) -> bool {
+//         false
+//     }
+
+//     fn changed(&mut self, ctx: &yew::Context<Mermaid>, props: &MermaidProperties) -> bool {
+//         match &self.props == props {
+//             true => false,
+//             false => {
+//                 self.props = props.clone();
+//                 true
+//             }
+//         }
+//     }
+
+//     fn view(&self, ctx: &Context<Mermaid>) -> Html {
+//         let drawer = self.create_drawer();
+//         let document = gloo_utils::document();
+//         let t = document.create_element("div").unwrap();
+//         t.set_inner_html(&drawer.render(&t, &self.props.code));
+//         Html::VRef(t.first_child().unwrap().into())
+//     }
+// }
+
+// impl Mermaid {
+//     pub fn create_drawer(&self) -> MermaidOptions {
+//         let mut render = MermaidOptions::default();
+//         render.set_theme(&self.props.theme);
+//         return render;
+//     }
+// }
